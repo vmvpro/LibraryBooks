77459f9dfe3a4f990b925516af72c1b7dc797581
vmvpro <melnvetal@gmail.com>
2020-05-23 11:19:10 +0300

    Migrations Add

---
+++ LibraryBooks/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace LibraryBooks.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LibraryBooks.Model.LibraryBooksContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(LibraryBooks.Model.LibraryBooksContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- LibraryBooks/Model/LibraryBooksContext.cs
+++ LibraryBooks/Model/LibraryBooksContext.cs
@@ -9,16 +9,19 @@ namespace LibraryBooks.Model
 {
     class LibraryBooksContext: DbContext
     {
-	    public LibraryBooksContext() :base("")
-	    {
-		    
-	    }
-
 	    public DbSet<Book> Books { get; set; }
 	    public DbSet<Author> Authors { get; set; }
 	    public DbSet<Category> Categories { get; set; }
 	    public DbSet<Subject> Subjects { get; set; }
 
+		public LibraryBooksContext() :base("LibraryBooksConnectionString")
+	    {
+			Database.SetInitializer(new MigrateDatabaseToLatestVersion<LibraryBooksContext, LibraryBooks.Migrations.Configuration>());
+		}
 
+	    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+	    {
+		    base.OnModelCreating(modelBuilder);
+	    }
 	}
 }
