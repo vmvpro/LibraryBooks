5d33f6d4bfdf2d0e60b149c5062e35c2606f146e
vmvpro <melnvetal@gmail.com>
2020-09-25 18:16:44 +0300

    Modification Subject model

--- LibraryBooks/Models/Subject.cs
+++ LibraryBooks/Models/Subject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryBooks.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,49 @@ using System.Threading.Tasks;
 
 namespace LibraryBooks.Model
 {
-public class Subject
+    public class Subject : Notifier
     {
-	    public int SubjectId { get; set; }
-	    public string Name { get; set; }
-	    public Category IdCategory { get; set; }
+
+        private int _subjectId;
+
+        public int SubjectId
+        {
+            get { return _subjectId; }
+            set
+            {
+                if (value == _subjectId) return;
+                _subjectId = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private Category _idCategory;
+
+        public Category IdCategory
+        {
+            get { return _idCategory; }
+            set
+            {
+                if (value == _idCategory) return;
+                _idCategory = value;
+                OnPropertyChanged();
+            }
+        }
+
     }
 }
