48007f825bdab7910ff03c8d7e6730abf3e663ca
vmvpro <melnvetal@gmail.com>
2020-09-25 15:52:41 +0300

    Add class Notifier : INotifyPropertyChanged

---
+++ LibraryBooks/Infrastructure/Notifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBooks.Infrastructure
+{
+    public class Notifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void NotifyPropertyChanged(
+            string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
