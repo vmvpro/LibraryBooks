3002104583442abe35ec26821df5d3cbe4e189d6
vmvpro <melnvetal@gmail.com>
2020-09-25 18:19:11 +0300

    Modification Category model

--- LibraryBooks/Models/Category.cs
+++ LibraryBooks/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryBooks.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,31 @@ using System.Threading.Tasks;
 
 namespace LibraryBooks.Model
 {
-    public class Category
+    public class Category : Notifier
     {
-	    public int CategoryId { get; set; }
-	    public string Name { get; set; }
+
+        private int _categoryId;
+        public int CategoryId
+        {
+            get { return _categoryId; }
+            set
+            {
+                if (value == _categoryId) return;
+                _categoryId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
