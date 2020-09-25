6ef23d0b07d6bf9776807a433b3c5bbe72c7a177
vmvpro <melnvetal@gmail.com>
2020-09-25 15:51:19 +0300

    Add class Command

---
+++ LibraryBooks/Commands/Command.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace LibraryBooks.Commands
+{
+    public class Command : ICommand
+    {
+        private Action methodToExecute = null;
+        private Func<bool> methodToDetectCanExecute = null;
+
+        
+        public Command(Action methodToExecute, Func<bool> methodToDetectCanExecute)
+        {
+            this.methodToExecute = methodToExecute;
+            this.methodToDetectCanExecute = methodToDetectCanExecute;
+
+            this.canExecuteChangedEventTimer = new DispatcherTimer();
+            this.canExecuteChangedEventTimer.Tick +=
+                canExecuteChangedEventTimer_Tick;
+            this.canExecuteChangedEventTimer.Interval = new TimeSpan(0, 0, 1);
+            this.canExecuteChangedEventTimer.Start();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.methodToDetectCanExecute == null)
+            {
+                return true;
+            }
+            else
+            {
+                return this.methodToDetectCanExecute();
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            this.methodToExecute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Реализует таймер, который может инициировать событие 
+        /// через указанные интервалы времени.
+        /// </summary>
+        private DispatcherTimer canExecuteChangedEventTimer = null;
+        void canExecuteChangedEventTimer_Tick(object sender, object e)
+        {
+            if (this.CanExecuteChanged != null)
+            {
+                this.CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
