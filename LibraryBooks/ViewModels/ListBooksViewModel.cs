c8f4b1572c00fe2d7cd6e2b3b2fae15f9263369d
vmvpro <melnvetal@gmail.com>
2020-09-25 15:55:08 +0300

    Add Views

---
+++ LibraryBooks/ViewModels/ListBooksViewModel.cs
@@ -0,0 +1,19 @@
+﻿using LibraryBooks.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBooks.ViewModels
+{
+    public class ListBooksViewModel
+    {
+        List<Book> listSelectionWork;
+
+        public ListBooksViewModel()
+        {
+                
+        }
+    }
+}
--- LibraryBooks/Views/MainWindow.xaml.cs
+++ LibraryBooks/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LibraryBooks.View
+namespace LibraryBooks.Views
 {
 	/// <summary>
 	/// Логика взаимодействия для MainWindow.xaml
