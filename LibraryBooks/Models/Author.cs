f74b9eb9dbb3f06c413eb3a550096a7edeb43d85
vmvpro <melnvetal@gmail.com>
2020-09-25 18:13:18 +0300

    Modification Author model

--- LibraryBooks/Models/Author.cs
+++ LibraryBooks/Models/Author.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryBooks.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,57 @@ using System.Threading.Tasks;
 
 namespace LibraryBooks.Model
 {
-    public class Author
+    public class Author : Notifier
     {
-	    public int AuthorId { get; set; }
-	    public string FirstName	{ get; set; }
-		public string LastName { get; set; }
-	    public string Comment { get; set; }
 
-	}
+        private int _authorId;
+        public int AuthorId
+        {
+            get { return _authorId; }
+            set
+            {
+                if (value == _authorId) return;
+                _authorId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (value == _firstName) return;
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (value == _lastName) return;
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _comment;
+        public string Comment
+        {
+            get { return _comment; }
+            set
+            {
+                if (value == _comment) return;
+                _comment = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
 }
