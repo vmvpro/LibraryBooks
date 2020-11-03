2adf14dbb99a03cb525b3931fd5b910d213fd338
vmvpro <melnvetal@gmail.com>
2020-11-03 13:11:35 +0200

    Added Converters

---
+++ LibraryBooks/Converters/ImageCoverter.cs
@@ -0,0 +1,23 @@
+﻿using LibraryBooksClient.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LibraryBooksClient.Converters
+{
+    public sealed class ImageConverter : IValueConverter
+    {
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return value != null ? ConvertImage.BytesToBitmapImage((byte[])value) : null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
